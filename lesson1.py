a7817e078bf54a220970eed3f95070c5721c3952
Levan <levanikoo454@bk.ru>
2022-09-28 13:23:59 +0400

    second commit

---
+++ lesson1.py
@@ -0,0 +1,41 @@
+name = "levani"
+
+print(len(name))       #len lenth
+         #4
+
+my_sentence =  "გამარჯობათ, შეგახსენებთ რომ სწავლას გააცდინო"
+
+#print(my_sentence)
+
+#knows_origraming = true
+
+
+
+
+print("hello")
+      #tab ის საშუალებით მოვახდინეთ ინდენტაცია (indentation)
+
+full_name = "levan tsagareishvili"
+print(len(full_name))
+
+#ყველა სტრინგი შეგვიძლიათ მივიჩნიოდ სიად
+#print(full_name[7])
+#position = index სინონიმებია
+
+full_name = "levan tsagareishvili"
+# print(full_name[2:9])
+# print(full_name[2:9:2])
+#start:finish:step
+
+# print(full_name[:5])
+# print(full_name[5:])
+
+print(full_name[-4:-1])
+
+
+
+
+
+
+
+
